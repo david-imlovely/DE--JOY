d42e08fc115cfffa15331f51628a7797f978caba
Tanimowo David <davidtanimowo01@gmail.com>
2023-04-02 14:57:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+function home() {
+    const element = document.getElementById("home");
+    element.scrollIntoView();
+  }
+
+function about() {
+    const element = document.getElementById("about");
+    element.scrollIntoView();
+  }
+
+function gallery() {
+  const element = document.getElementById("gallery");
+  element.scrollIntoView();
+}
+
+function contact() {
+  const element = document.getElementById("contact");
+  element.scrollIntoView();
+}
